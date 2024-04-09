06b58ea3b4600785040bd145ea7ac06db1ad7a1d
d-kens <omondikens1@gmail.com>
2024-04-09 05:31:05 +0300

    variable scope

--- 4_functions/main.js
+++ 4_functions/main.js
@@ -2,6 +2,8 @@
 - function: A block of reusable code. Declare code once and use it whenever you want.
 - Declare a function once and use whenever you want.
 - parameters, arguements, return type
+
+- Variable scope: Where a variable is recognized and accessible (local and global).
 */
 
 function isEven(number) {
@@ -20,3 +22,16 @@ function isValidEmail(email) {
 console.log(isValidEmail("omondikens@gmail.com"));
 console.log(isValidEmail("issss"));
 console.log(isValidEmail("dickens.onyango@gmail.com"));
+
+
+// NB: When inside a function we will use any local instance of a variable first, if it is not found we look at the global scope
+let x = 10
+let y = 11;
+
+function function1() {
+    let x = 20;
+    console.log(x);
+    console.log(y)
+}
+
+function1();
