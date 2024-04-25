f859d6fa4f242ede9bd726e2d14889821afcce19
d-kens <omondikens1@gmail.com>
2024-04-25 05:00:50 +0300

    sort method

--- 18_sort/main.js
+++ 18_sort/main.js
@@ -14,5 +14,23 @@ console.log(strings);
 
 
 let numbers = [1, 10, 2, 9, 3, 8, 4, 7, 5, 6];
-numbers.sort();
+numbers.sort((a, b) => a - b);
 console.log(numbers);
+numbers.sort((a, b ) => b - a); // reverse order
+console.log(numbers);
+
+
+const people = [
+    { name: "spongebob", age: 30, gpa: 3.0 },
+    { name: "patrick", age: 37, gpa: 1.5 },
+    { name: "squidward", age: 51, gpa: 2.5 },
+    { name: "sandy", age: 27, gpa: 4.0 },
+]
+
+people.sort((a, b) => a.age - b.age); 
+console.log(people);
+people.sort((a, b) => b.age - a.age);
+console.log(people);
+
+people.sort((a, b) => a.name.localeCompare(b.name));
+console.log(people);
