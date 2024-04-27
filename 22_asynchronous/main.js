fd2b8e823058961db36475e398083b6b9cf2ea48
d-kens <omondikens1@gmail.com>
2024-04-27 14:10:50 +0300

    handling async functions

--- 22_asynchronous/main.js
+++ 22_asynchronous/main.js
@@ -1,17 +1,23 @@
 /*
  - Synchronous code executes line by line consecutively in a sequential manner.
  - Asynchronous code allows multiple operations to be performed concurrently without blocking the main execution thread.
- - Async operations include I/O operations, network requests, fetching data, or any task that could take an indeterminate amount of time.
  - Asynchronous code is typically handled with callbacks, promises, or async/await syntax.
+ - Asynchronous operations are operations that take an indeterminate amount of time to complete. Examples are
+    1. I/O operations
+    2. Network requests
+    3. Fetching data from a server
+
+
+  Explanantions
+   - Asyc code doesn't block the main excution thread. They are exexcuted asynchronously, allowing the program to
+   continue with other tasks while waiting for the asynchrous operation operation to complete.
+   - They don't halt the excution of other tasks.
+   - This means that if there are tasks that need to be performed after and async operation completes, we can't rely on sequential execution.
+   - Callback (and Promises and Async/Await) are a way to handle async operations by providing a mechanism for executing code after an asyc operation completes.
 */
 
-/*
-    Handling async code using callbacks
-     - function1 is asynchronous.
-     - function2 is synchronous.
-     - We use a callback to ensure tasks 2 to 5 execute after task one is complete.
-*/
-
+// callback passed as arguement to asyn functions
+// callback serve as a way of specifying what should happen once the asyn task completes
 function function1(callback) {
     setTimeout(() => {console.log("Task 1"); callback()}, 3000);
 }
