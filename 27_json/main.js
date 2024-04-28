9f3b5193b4e873e5b8f1cef084f82e767804641f
d-kens <omondikens1@gmail.com>
2024-04-28 12:40:21 +0300

    JSON

---
+++ 27_json/main.js
@@ -0,0 +1,5 @@
+/*
+ - JSON (JavaScript Object Notation): data exchange format used for exchanging data between a serve and a web application.
+ - JSON.stringfy(): convert a JS object to JSON string.
+ - JSON.parse(): converts a JSON string to a JS object.
+*/
